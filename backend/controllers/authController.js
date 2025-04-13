fcd0dfe6439e5313cb13dfac8e61586f66cb981b
Ajay Kumar Saraswat <ajaykumarsaraswat9045@gmail.com>
2025-04-13 18:57:21 +0530

    register the user

---
+++ backend/controllers/authController.js
@@ -0,0 +1,33 @@
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+};
+
+const registerUser = async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const userExists = await User.findOne({ $or: [{ email }, { username }] });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({
+      username,
+      email,
+      password,
+    });
+    if (user) {
+      res.status(201).json({
+        _id: user._id,
+        username: user.username,
+        email: user.email,
+        token: generateToken(user._id),
+      });
+    }
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+};
